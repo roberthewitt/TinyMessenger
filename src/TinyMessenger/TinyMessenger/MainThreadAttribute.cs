575372996629a2f64f923f041e848564ef710ce0
jock <jock.findlay@fanduel.com>
2015-10-29 17:23:47 +0000

    Add MainThread attribute - does nothing

---
+++ src/TinyMessenger/TinyMessenger/MainThreadAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TinyMessenger {
+    /// <summary>
+    /// Specify alongside the Subscribe attribute so that messages will be delivered to the method on the main thread.
+    /// Client applications must specify the main-thread message proxy.
+    /// 
+    /// <example>
+    /// public class ConsoleLogger
+    /// {
+    ///     [Subscribe, MainThread]
+    ///     public void OnPurchase(IPurchaseEvent purchase)
+    ///     {
+    ///         Console.WriteLine(purchase.GetAmount() + " on main thread");
+    ///     }
+    /// }
+    /// </example>
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Method, AllowMultiple = false)]
+    public class MainThread : Attribute {
+    }
+}
